72d5b0c7069ab7ba53d66196270740ad9771300a
dave <dave@xiala.net>
2021-04-20 01:57:34 +0200

    New df data_accident_cleaned. Updated README.md

--- data/tidy/README.md
+++ data/tidy/README.md
@@ -11,3 +11,31 @@ This is the folder for the tidy dataset. Defined as this:
 • use descriptive names (e.g., AgeAtDiagnosis, not ADx)
 
 • CSV or tab-delimited text files work great
+
+<h3>Features:</h3>
+
+<h4>AccidentType</h4>
+0: Accident with skidding or self-accident
+1: Accident when overtaking or changing lanes
+2: Accident with rear-end collision
+3: Accident when turning left or right
+4: Accident when turning-into main road
+5: Accident when crossing the lane(s)
+6: Accident with head-on collision
+7: Accident when parking
+8: Accident involving pedestrian(s)
+9: Accident involving animal(s)
+
+<h4>AccidentSeverityCategory</h4>
+1: Accident with fatalities
+2: Accident with severe injuries
+3: Accident with light injuries
+4: Accident with property damage
+
+<h4>RoadType</h4>
+0: Motorway
+1: Expressway
+2: Principal road
+3: Minor road
+4: Motorway side installation
+9: Other
--- data/tidy_up_data.py
+++ data/tidy_up_data.py
@@ -5,6 +5,7 @@ Created on Thu Apr 15 16:30:42 2021
 """
 
 import pandas as pd
+import numpy as np
 
 # =============================================================================
 # Raw data
@@ -26,21 +27,17 @@ features_original_accident = ['AccidentUID', 'AccidentType', 'AccidentType_de',
                      'AccidentWeekDay_fr', 'AccidentWeekDay_it', 'AccidentWeekDay_en',
                      'AccidentHour', 'AccidentHour_text']
 
-features_croped_accident = ['AccidentUID', 'AccidentType', 'AccidentType_en',
-                     'AccidentSeverityCategory', 'AccidentSeverityCategory_en',
-                     'AccidentInvolvingPedestrian', 'AccidentInvolvingBicycle',
-                     'AccidentInvolvingMotorcycle', 'RoadType', 'RoadType_en',
-                     'AccidentLocation_CHLV95_E', 'AccidentLocation_CHLV95_N',
-                     'CantonCode', 'MunicipalityCode', 'AccidentYear',
-                     'AccidentMonth','AccidentMonth_en', 'AccidentWeekDay',
-                     'AccidentWeekDay_en', 'AccidentHour', 'AccidentHour_text']
+features_croped_accident = ['AccidentType',
+                            'AccidentSeverityCategory', 'AccidentInvolvingPedestrian', 'AccidentInvolvingBicycle',
+                            'AccidentInvolvingMotorcycle', 'RoadType', 'AccidentLocation_CHLV95_E',
+                            'AccidentLocation_CHLV95_N', 'AccidentYear', 'AccidentMonth', 'AccidentWeekDay',
+                            'AccidentHour']
 
 features_meteo = ['Datum', 'Standort', 'Parameter', 'Intervall', 'Einheit',
                   'Wert', 'Status']
 # =============================================================================
 # Tidy data functions
 # =============================================================================
-
 def to_int(data, feature_list, m, k):
     """
     Removes unwanted string part of the data.
@@ -62,28 +59,25 @@ def to_int(data, feature_list, m, k):
 
     """
 
-    integerfy = lambda lst, i: [int(item[i:]) for item in  lst]
+    integerfy = lambda lst, i: [int(item[i:]) for item in lst]
     data_arr = data[feature_list[m]].to_numpy()
     temp = integerfy(data_arr, k)
-    data[feature_list[m]] = temp # make str of form "str" + "number" to number
+    data[feature_list[m]] = temp  # make str of form "str" + "number" to number
+
 
 # =============================================================================
 # Perform cleaning
 # =============================================================================
 # clean accident data
-to_int(data_accident, features_croped_accident, 1, 2)  # type
-to_int(data_accident, features_croped_accident, 3, 2)  # severity
-to_int(data_accident, features_croped_accident, 8, 2)  # road type
-to_int(data_accident, features_croped_accident, 17, 2) # week day
+data_accident_cleaned = data_accident[features_croped_accident].copy()  # complete new df with only used features
+nan_index = data_accident_cleaned[data_accident_cleaned.isin([np.nan, np.inf, -np.inf]).any(1)].index  # get indices from nan values
+data_accident_cleaned.drop(nan_index, inplace=True)  # drop nan values
+data_accident_cleaned[features_original_accident[11:14]] = data_accident_cleaned[features_original_accident[11:14]].astype(int)  # Changing bool to int
+data_accident_cleaned[features_original_accident[34]] = data_accident_cleaned[features_original_accident[34]].astype(int)  # change weekday to int
+to_int(data_accident_cleaned, features_original_accident, 1, 2)  # type
+to_int(data_accident_cleaned, features_original_accident, 6, 2)  # severity
+to_int(data_accident_cleaned, features_original_accident, 14, 4)  # road type
+to_int(data_accident_cleaned, features_original_accident, 29, 3)  # week day
 
-for i in range(3):
-    del data_accident[features_original_accident[2+i]]    # delet type de, fe, it
-    del data_accident[features_original_accident[7+i]]    # delet severity de, fe, it
-    del data_accident[features_original_accident[15+i]]   # delet severity de, fe, it
-    del data_accident[features_original_accident[25+i]]   # delet month de, fe, it
-    del data_accident[features_original_accident[30+i]]   # delet week day de, fe, it
-
-data_accident.to_csv("tidy/RoadTrafficAccidentLocations_cleaned.csv")
+data_accident_cleaned.to_csv("tidy/RoadTrafficAccidentLocations_cleaned.csv")
 # =============================================================================
-
-

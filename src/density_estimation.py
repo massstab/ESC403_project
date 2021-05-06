c5b7482904ab7ee410d2f3d662f808debaf71b0b
marszpd <45564321+marszpd@users.noreply.github.com>
2021-05-06 17:43:23 +0200

    made some minor changes in density_estimation.py

--- src/density_estimation.py
+++ src/density_estimation.py
@@ -14,7 +14,7 @@ from statsmodels.tsa.arima.model import ARIMA
 from matplotlib.image import imread
 from helpers import lv95_latlong
 import seaborn as sns
-import KernelDensity
+from sklearn.neighbors import KernelDensity
 
 
 df = pd.read_csv('../data/tidy_data/data_merged.csv')[:1000]
@@ -43,14 +43,14 @@ map01 = imread("../data/Zürich_map/map.png")
 # =============================================================================
 # Display whole data st
 # =============================================================================
-fig, ax = plt.subplots(figsize=(15, 12), dpi=80)
-ax.scatter(longitude, latitude, zorder=1, alpha=0.3,  s=2, c="b")
-ax.set_xlim(BBox[0], BBox[1])
-ax.set_ylim(BBox[2], BBox[3])
-ax.set_title('Accidents Spatial Data')
-ax.imshow(map01, zorder=0, extent=BBox, aspect='equal')
-plt.autoscale()
-plt.show()
+# fig, ax = plt.subplots(figsize=(15, 12), dpi=80)
+# ax.scatter(longitude, latitude, zorder=1, alpha=0.3,  s=2, c="b")
+# ax.set_xlim(BBox[0], BBox[1])
+# ax.set_ylim(BBox[2], BBox[3])
+# ax.set_title('Accidents Spatial Data')
+# ax.imshow(map01, zorder=0, extent=BBox, aspect='equal')
+# plt.autoscale()
+# plt.show()
 
 # sns.jointplot(longitude[:,0], latitude[:,0])
 # =============================================================================
@@ -71,10 +71,11 @@ positions = np.vstack([X.ravel(), Y.ravel()])
 # plt.show()
 
 # via KernelDensity
-kde = KernelDensity(bandwidth=0.04, metric='haversine', kernel='gaussian', algorithm='ball_tree')
+kde = KernelDensity(bandwidth=0.04, kernel='gaussian', algorithm='ball_tree')
 kde.fit(samples)
 
 Z = kde.score_samples(positions)
+Z = Z.reshape(X.shape)
 # plot contours of the density
 levels = np.linspace(0, Z.max(), 25)
 plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)

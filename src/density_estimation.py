f0267b5433e1f9b9a44a2a2459137166d3dc45c9
marszpd <45564321+marszpd@users.noreply.github.com>
2021-05-07 10:35:03 +0200

    added 5 SSP_proposal.* files

--- src/density_estimation.py
+++ src/density_estimation.py
@@ -17,7 +17,7 @@ import seaborn as sns
 from sklearn.neighbors import KernelDensity
 
 
-df = pd.read_csv('../data/tidy_data/data_merged.csv')[:1000]
+df = pd.read_csv('../data/tidy_data/data_merged.csv')[:100000]
 # df_count_ped_bike = pd.read_pickle('../data/tidy_data/pre_tidy_fussgaenger_velo/2011-2020_verkehrszaehlungen_werte_fussgaenger_velo_cleaned.pickle')
 # df_count_car = pd.read_csv('../data/tidy_data/pre_tidy_auto/sid_dav_verkehrszaehlung_miv_OD2031_2012-2020.csv')
 
@@ -61,21 +61,25 @@ samples = np.vstack([longitude[:,0], latitude[:,0]]) # format data
 X, Y = np.mgrid[BBox[0]:BBox[1]:1000j, BBox[2]:BBox[3]:1000j]
 positions = np.vstack([X.ravel(), Y.ravel()])
 
-# # via scipy.stats
-# kernel = stats.gaussian_kde(samples)
-# Z = np.reshape(kernel(positions).T, X.shape)
+# via scipy.stats
+kernel = stats.gaussian_kde(samples)
+Z = np.reshape(kernel(positions).T, X.shape)
 
-# fig, ax = plt.subplots()
-# ax.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r, extent=BBox)
-# ax.plot(longitude, latitude, 'k.', markersize=2)
+fig, ax = plt.subplots()
+ax.imshow(np.rot90(Z), cmap=plt.cm.gist_earth_r, extent=BBox)
+ax.plot(longitude, latitude, 'k.', markersize=2)
+plt.show()
+
+# # via KernelDensity
+# kde = KernelDensity(bandwidth=0.00004, kernel='gaussian', algorithm='ball_tree')
+# print(samples.shape)
+# kde.fit(samples.T)
+# print(positions.shape)
+
+# Z = kde.score_samples(positions.T)
+# Z = Z.reshape(X.shape)
+# # plot contours of the density
+# levels = np.linspace(0, Z.max(), 25)
+# plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
+# plt.scatter(longitude, latitude, s=2)
 # plt.show()
-
-# via KernelDensity
-kde = KernelDensity(bandwidth=0.04, kernel='gaussian', algorithm='ball_tree')
-kde.fit(samples)
-
-Z = kde.score_samples(positions)
-Z = Z.reshape(X.shape)
-# plot contours of the density
-levels = np.linspace(0, Z.max(), 25)
-plt.contourf(X, Y, Z, levels=levels, cmap=plt.cm.Reds)
